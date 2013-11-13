a1fc613288455407df386834961bb52114740c59
Justin Rhodes <justin@westwerkdesign.com>
2013-11-13 00:35:30 -0600

    JS Scripts

--- inc/scripts.php
+++ inc/scripts.php
@@ -11,7 +11,7 @@ function starter_scripts() {
 
 	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js' );
 
-	wp_enqueue_script( 'alpha-bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '0.0.1', true );
+	wp_enqueue_script( 'alpha-bootstrap', get_template_directory_uri() . '/assets/js/scripts.js', array(), '0.0.1', true );
 
 	wp_enqueue_script( 'alpha-custom', get_template_directory_uri() . '/assets/js/custom.js', array(), '0.0.1', true );
 
